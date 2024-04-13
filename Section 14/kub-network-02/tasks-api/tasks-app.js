fa1294434d3e1764964616676d8eae82fbdfa847
deep-bansal <deepvinaybansal@yahoo.com>
2024-04-13 18:17:11 +0530

    multi deployment done

--- Section 14/kub-network-02/tasks-api/tasks-app.js
+++ Section 14/kub-network-02/tasks-api/tasks-app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
-const filePath = path.join(__dirname, process.env.TASKS_FOLDER, 'tasks.txt');
+const filePath = path.join(__dirname, 'tasks.txt');
 
 const app = express();
 
@@ -17,7 +17,7 @@ const extractAndVerifyToken = async (headers) => {
   }
   const token = headers.authorization.split(' ')[1]; // expects Bearer TOKEN
 
-  const response = await axios.get('http://auth/verify-token/' + token);
+  const response = await axios.get(`http://${process.env.AUTH_ADDRESS}/verify-token/` + token);
   return response.data.uid;
 };
 
@@ -52,7 +52,7 @@ app.post('/tasks', async (req, res) => {
     fs.appendFile(filePath, jsonTask + 'TASK_SPLIT', (err) => {
       if (err) {
         console.log(err);
-        return res.status(500).json({ message: 'Storing the task failed.' });
+        return res.status(500).json({ message: err });
       }
       res.status(201).json({ message: 'Task stored.', createdTask: task });
     });
